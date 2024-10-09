75819ad3e815eb207857ba550ffe646506945db1
eirolnepo <eiroljohn35@gmail.com>
2024-10-10 00:42:36 +0800

    finished homepage design with modal animations and dark mode

---
+++ js/script.js
@@ -0,0 +1,102 @@
+const modal = document.getElementById('sign-in-modal');
+const signInBtn = document.getElementById('sign-in-btn');
+const signUpModal = document.getElementById('sign-up-modal');
+const signUpBtn = document.getElementById('sign-up-btn');
+const signInUpBtn = document.getElementById('sign-in-up-btn');
+const signUpInBtn = document.getElementById('sign-up-in-btn');
+const changePassModal = document.getElementById('change-pass-modal');
+const forgotPassLink = document.getElementById('forgot-pass-link');
+const changePassInBtn = document.getElementById('change-pass-in-btn');
+const darkModeButton = document.getElementById('nav-dark-mode');
+
+darkModeButton.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    
+    if (document.body.classList.contains('dark-mode')) {
+      darkModeButton.src = 'imgs/dark-mode-white.png';
+    } else {
+      darkModeButton.src = 'imgs/dark-mode-green.png';
+    }
+});
+
+function showModal(modal) {
+    modal.style.display = 'flex';
+    modal.style.justifyContent = 'center';
+    modal.style.alignItems = 'center';
+    modal.classList.add('fade-in');
+
+    setTimeout(() => {
+        modal.classList.remove('fade-in');
+    }, 300);
+}
+
+function hideModal(modal) {
+    modal.classList.add('fade-out');
+
+    setTimeout(() => {
+        modal.style.display = 'none';
+        modal.classList.remove('fade-out');
+    }, 300);
+}
+
+signInBtn.addEventListener('click', function() {
+    showModal(modal);
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+        hideModal(modal);
+    }
+});
+
+signUpBtn.addEventListener('click', function() {
+    showModal(signUpModal);
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == signUpModal) {
+        hideModal(signUpModal);
+    }
+});
+
+signInUpBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    hideModal(modal);
+
+    setTimeout(() => {
+        showModal(signUpModal);
+    }, 300);
+});
+
+signUpInBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    hideModal(signUpModal);
+
+    setTimeout(() => {
+        showModal(modal);
+    }, 300);
+});
+
+forgotPassLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    hideModal(modal);
+
+    setTimeout(() => {
+        showModal(changePassModal);
+    }, 300);
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == changePassModal) {
+        hideModal(changePassModal);
+    }
+});
+
+changePassInBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    hideModal(changePassModal);
+
+    setTimeout(() => {
+        showModal(modal);
+    }, 300);
+});
