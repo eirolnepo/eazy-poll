194023f562ee170e0a704dd4c0ef9e108163d599
eirolj <eiroljohn35@gmail.com>
2024-11-05 22:14:00 +0800

    designed the thank you page

--- creator/js/thankyou-page.js
+++ creator/js/thankyou-page.js
@@ -11,19 +11,8 @@ for (let i = 0; i < textareas.length; i++) {
 
 document.addEventListener("DOMContentLoaded", function () {
     const darkModeButton = document.getElementById("nav-dark-mode");
-    const addQuestionBtn = document.querySelector("#add-question-btn");
-    const surveyContainer = document.querySelector("#survey-container");
-    const addOptionsBtn = document.querySelector("#add-options-btn");
-    const addOptionsContainer = document.querySelector("#add-options-container");
-    const addTdBtn = document.querySelector("#add-td-btn");
-    const addImageBtn = document.querySelector("#add-image-btn");
-    const addSaveImage = document.getElementById("add-save-btn");
 
     let questionCounter = 0;
-
-    addOptionsContainer.style.display = "none";
-    addOptionsContainer.style.opacity = 0;
-    addOptionsContainer.style.transform = "scale(0.9)";
 
     function updateImagesForDarkMode() {
         const darkModeEnabled = localStorage.getItem("darkMode") === "enabled";
@@ -35,10 +24,6 @@ document.addEventListener("DOMContentLoaded", function () {
         const uploadImageBtns = document.getElementsByClassName("upload-image-btn");
 
         darkModeButton.src = darkModeEnabled ? "../imgs/dark-mode-white.png" : "../imgs/dark-mode-green.png";
-        addQuestionBtn.src = darkModeEnabled ? "../imgs/plus_choices_dark.svg" : "../imgs/plus_choices.svg";
-        addTdBtn.src = darkModeEnabled ? "../imgs/text_dark.svg" : "../imgs/text_logo.svg";
-        addImageBtn.src = darkModeEnabled ? "../imgs/image_dark.svg" : "../imgs/image_logo.svg";
-        addSaveImage.src = darkModeEnabled ? "../imgs/save_dark.svg" : "../imgs/save.svg";
 
         const addChoiceSrc = darkModeEnabled ? "../imgs/plus_choices_dark.svg" : "../imgs/plus_choices.svg";
         const deleteChoiceSrc = darkModeEnabled ? "../imgs/close_dark.svg" : "../imgs/close.svg";
@@ -184,115 +169,6 @@ document.addEventListener("DOMContentLoaded", function () {
             }, 300);
         });
     }
-
-    addTdBtn.addEventListener("click", function () {
-        const titleDescContainer = document.createElement("div");
-        titleDescContainer.classList.add("new-title-desc-container");
-
-        titleDescContainer.innerHTML = `
-            <input type="text" name="survey-title" class="survey-title" value="Untitled Section">
-            <textarea name="survey-desc" class="survey-desc" placeholder="Section Description"></textarea>
-            <img src="../imgs/delete.svg" alt="Delete section button" class="delete-section-btn">
-        `;
-        surveyContainer.appendChild(titleDescContainer);
-        updateImagesForDarkMode();
-
-        const newTextarea = titleDescContainer.querySelector(".survey-desc");
-        newTextarea.style.resize = "none";
-
-        newTextarea.addEventListener("input", function() {
-            this.style.height = "auto";
-            this.style.height = this.scrollHeight + "px";
-        });
-
-        setTimeout(() => {
-            titleDescContainer.classList.add("show");
-        }, 10);
-
-        const deleteSectionBtn = titleDescContainer.querySelector(".delete-section-btn");
-        deleteSectionBtn.addEventListener("click", function () {
-            titleDescContainer.classList.remove("show");
-
-            setTimeout(() => {
-                titleDescContainer.remove();
-            }, 300);
-        });
-        scrollToBottom();
-    });
-
-    addImageBtn.addEventListener("click", function () {
-        const imageUploadDiv = document.createElement("div");
-        imageUploadDiv.classList.add("image-upload-container");
-        imageUploadDiv.innerHTML = `
-            <p class="upload-text"><img src="../imgs/upload.svg" alt="Upload button" class="upload-image-btn">Click to upload an image</p>
-            <img src="../imgs/delete.svg" alt="Delete button" class="delete-upload-btn">
-        `;
-    
-        const imageInput = document.createElement("input");
-        imageInput.type = "file";
-        imageInput.accept = "image/*";
-        imageInput.style.display = "none";
-    
-        imageUploadDiv.appendChild(imageInput);
-    
-        imageUploadDiv.addEventListener("click", function () {
-            imageInput.click();
-        });
-    
-        imageInput.addEventListener("change", function () {
-            const file = imageInput.files[0];
-            if (file) {
-                const reader = new FileReader();
-                reader.onload = function (e) {
-                    const img = document.createElement("img");
-                    img.src = e.target.result;
-                    img.classList.add("uploaded-image");
-                    img.style.maxWidth = "100%";
-                    img.style.maxHeight = "100%";
-    
-                    imageUploadDiv.innerHTML = "";
-                    imageUploadDiv.appendChild(img);
-    
-                    const deleteBtn = document.createElement("img");
-                    deleteBtn.classList.add("delete-upload-btn");
-    
-                    const darkModeEnabled = localStorage.getItem("darkMode") === "enabled";
-                    deleteBtn.src = darkModeEnabled ? "../imgs/delete_dark.svg" : "../imgs/delete.svg";
-                    deleteBtn.alt = "Delete button";
-    
-                    deleteBtn.addEventListener("click", function (event) {
-                        event.stopPropagation();
-                        imageUploadDiv.classList.remove("show");
-    
-                        setTimeout(() => {
-                            imageUploadDiv.remove();
-                        }, 300);
-                    });
-    
-                    imageUploadDiv.appendChild(deleteBtn);
-                };
-                reader.readAsDataURL(file);
-            }
-        });
-    
-        imageUploadDiv.querySelector(".delete-upload-btn").addEventListener("click", function (event) {
-            event.stopPropagation();
-            imageUploadDiv.classList.remove("show");
-    
-            setTimeout(() => {
-                imageUploadDiv.remove();
-            }, 300);
-        });
-    
-        surveyContainer.appendChild(imageUploadDiv);
-    
-        updateImagesForDarkMode();
-    
-        setTimeout(() => {
-            imageUploadDiv.classList.add("show");
-        }, 10);
-        scrollToBottom();
-    });
     
     const existingQuestions = document.querySelectorAll(".question-container");
     let existingquestionCounter = 0;
@@ -322,11 +198,5 @@ document.addEventListener("DOMContentLoaded", function () {
             event.preventDefault();
             alert("Please select at least one checkbox.");
         }
-    });
-
-    document.getElementById('clear-btn').addEventListener('click', function(){
-        const id = document.getElementById("user_id").value;
-        const survey_id = document.getElementById("survey_id").value;
-        window.location.href = `response-page.php?id=${id}&survey_id=${survey_id}`;
     });
 });
